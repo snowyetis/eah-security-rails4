3b4d02f8b90da72098da8e584b928c207dc5e175
Joe <josephd.carlson@gmail.com>
2017-01-19 01:57:23 -0500

    Again this schema

---
+++ db/migrate/20170119065032_create_questionaire_table.rb
@@ -0,0 +1,12 @@
+class CreateQuestionaireTable < ActiveRecord::Migration
+  def change
+      create_table :questionaires do |t|
+        t.references :user, index: true, foreign_key: true
+        t.references :quote, index: true, foreign_key: true
+        t.string :product_type
+        t.text :comments
+
+        t.timestamps null: false
+      end
+    end
+  end
