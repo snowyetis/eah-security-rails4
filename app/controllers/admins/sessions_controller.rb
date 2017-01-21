a598f8dd0fc6d81e9fda2856f91ad958edfd68a4
Joe <josephd.carlson@gmail.com>
2017-01-21 00:38:12 -0500

    Removed for from sesssions param setter

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -30,7 +30,7 @@ before_filter :configure_sign_in_params, only: [:create]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.for(:sign_in) << :attribute
+    devise_parameter_sanitizer.permit(:sign_in)
   end
 
 end
