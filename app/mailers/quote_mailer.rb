83721655f4323dca52b6ca1d9b492f5cd238b72c
Joe <josephd.carlson@gmail.com>
2017-01-21 05:38:58 -0500

    Fixed localhost ref in mailers

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -3,19 +3,19 @@ class QuoteMailer < ApplicationMailer
   # TODO: Complete quote e-mail notifications
   def fire_door_quote_request(quote)
     @quote = quote
-    @url = "http://localhost:3000/quotes"
+    @url =  request.original_url + "/quotes"
 
   end
 
   def security_solutions_quote_request(quote)
     @quote = quote
-    @url = "http://localhost:3000/quotes"
+    @url =  request.original_url + "/quotes"
 
   end
 
   def locksmith_quote_request(quote)
     @quote = quote
-    @url = "http://localhost:3000/quotes"
+    @url = request.original_url + "/quotes"
     
   end
 
