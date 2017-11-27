6cf40c2c39e5acddef83d5bb30847ce03a46a509
Joe <josephd.carlson@gmail.com>
2017-11-27 08:45:09 -0500

    Dynamic products

--- db/migrate/20170820013759_add_reference_requester_type_id_to_quote.rb
+++ db/migrate/20170820013759_add_reference_requester_type_id_to_quote.rb
@@ -1,5 +1,5 @@
 class AddReferenceRequesterTypeIdToQuote < ActiveRecord::Migration
   def change
-    add_reference :quotes, :requester_type_id, index: true, foreign_key: true
+    add_reference :quotes, :requester_type, index: true, foreign_key: true
   end
 end
