daef5f50d63f8f05504394897bbe81881e98c4d6
Joe <josephd.carlson@gmail.com>
2018-12-27 22:53:41 -0500

    Ugh

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'bundler/setup'
 
 ENV['RAILS_ENV'] ||= 'production'
 # Initialize the Rails application.
-# Rails.application.initialize!
+Rails.application.initialize!
